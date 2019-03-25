30e1feb5b39d98cb9e90720aa117d02a408fc52d
Unknown <anthony.smiderle@uoit.net>
2019-03-24 22:07:48 -0400

    yes

--- lePacMan/Classes/Level.cpp
+++ lePacMan/Classes/Level.cpp
@@ -1,5 +1,5 @@
 #include "Level.h"
-
+#define BOXSIZE 15
 DevitoCult::Level::Level()
 {
 }
@@ -9,9 +9,9 @@ void DevitoCult::Level::drawMap(cocos2d::Scene * s)
 	for (int i = 0; i < 21; i++) {
 		for (int j = 0; j < 27; j++) {
 			if (levelData[i][j] == 1) {
-				levelBounds.push_back(SquarePrimitive(cocos2d::Vec2(10, 10), cocos2d::Vec2(20, 20)));
+				levelBounds.push_back(SquarePrimitive(cocos2d::Vec2(0, 0), cocos2d::Vec2(BOXSIZE, BOXSIZE)));
 				s->addChild(levelBounds.back().getDrawNode());
-				levelBounds.back().getDrawNode()->setPosition(cocos2d::Vec2(10 + (j * 10), 270 - (i * 10)));
+				levelBounds.back().getDrawNode()->setPosition(cocos2d::Vec2(40 + (j * BOXSIZE), 303 - (i * BOXSIZE)));
 				levelBounds.back().setColour(cocos2d::Color4F(0, 0, 1.0f, 1.0f));
 			}
 
