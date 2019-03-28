4af361481d42284e562ed30aae8afc575f1a43c6
CraigHolder <craig.holder@uoit.net>
2019-03-27 21:31:27 -0400

    Score

--- lePacMan/Classes/HelloWorldScene.h
+++ lePacMan/Classes/HelloWorldScene.h
@@ -48,6 +48,11 @@ public:
 	DevitoCult::Level levelOne;
 	DevitoCult::XinputManager manager;
 	DevitoCult::XinputController* p1Controller;
+	//value to track the number of pac dots eaten
+	int numPacDots;
+	//score and the labe to display it
+	int score;
+	cocos2d::Label * sLabel;
 	//controller sticks
 	DevitoCult::Stick p1Sticks[2];
 	DevitoCult::Pacman* pacman;
